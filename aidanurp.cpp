3916a1b2a6b4f23ff5817e71dbd6cbcc4ff2bb14
aidanurp <14maret1998@gmail.com>
2018-01-14 22:07:30 +0700

    selesai dehhh

--- aidanurp.cpp
+++ aidanurp.cpp
@@ -9,5 +9,25 @@ class parfume{
 };
 
 int main (){
+	parfume bodymist, EDP, EDT;
+	//EDP : eu de parfume
+	//EDT : eu de toilette
 	
+	bodymist.merk = "Victoria Secrets";
+	EDP.merk = "Bath and Body Works";
+	EDT.merk = "The Body Shop";
+	
+	cout << (bodymist.merk);
+	bodymist.baunya("enak");
+	
+	cout << (EDP.merk);
+	EDP.baunya("wangi");
+	
+	cout << (EDT.merk);
+	EDT.baunya("segar");
+		
 }
+void parfume::baunya(string harum){
+	cout<<"Haruumya"<<harum<<"sekali \n";
+}	
+
